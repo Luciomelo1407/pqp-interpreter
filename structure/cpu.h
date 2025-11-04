44e7426186b2fea751d495ef67ca6ce26eecaa21
luciomelo1407 <luciomelo90@gmail.com>
2025-11-04 09:32:24 -0300

    feat: created new instructions counter

--- structure/cpu.h
+++ structure/cpu.h
@@ -5,17 +5,12 @@
 #ifndef CPU_H
 #define CPU_H
 
-typedef struct{
-  int instruction;
-  int counter;
-}InstructionCounter ;
-
 typedef struct {
     int16_t registers[16];
     uint8_t mem[256];
     bool g, l, e;
     int32_t pc;
-    
+    int instructionsCounter[16];
     FILE *output;
 } Cpu;
 
