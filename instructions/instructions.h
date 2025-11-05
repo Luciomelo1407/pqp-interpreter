fb7a52775b1bf19870a9b0e025a4ca9154e450fd
luciomelo1407 <luciomelo90@gmail.com>
2025-11-05 13:24:51 -0300

    feat: cmp, sar implmented not tested

--- instructions/instructions.h
+++ instructions/instructions.h
@@ -1,15 +1,21 @@
 #include "../structure/cpu.h"
+#include <stdint.h>
 #ifndef instructions_h
 #define instructions_h
 /*
  *  Ana    -> 4*movs
- *  Lúcio  -> cmp, -jmp-, sar
+ *  Lúcio  -> *cmp*, -jmp-, *sar* // TODO revisar asteriscos
  *  Maiara -> add, sub, and
- *  Paulo  -> jmpg, jmpl, jmpe
+ *  Paulo  -> jmpg, jmpl, jmpe //TODO revisar isso aqui assing: Lúcio
  *  Rafa   -> xor, or, sal
  * */
 
+void cmp(Cpu* cpu,uint8_t firstField, uint8_t secondField );
 void jmp(Cpu* cpu, uint16_t thirdField);
+void jmp_g(Cpu* cpu, uint16_t thirdField);
+void jmp_l(Cpu* cpu, uint16_t thirdField);
+void jmp_e(Cpu* cpu, uint16_t thirdField);
+void sar(Cpu* cpu,uint8_t firstField, uint16_t thirdField);
 int exit_setup(Cpu* cpu);
 
 #endif
