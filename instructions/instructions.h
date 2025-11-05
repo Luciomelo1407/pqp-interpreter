2a81fa5fd1817a388967c639491f5898d69607b1
luciomelo1407 <luciomelo90@gmail.com>
2025-11-05 15:08:50 -0300

    fead: made add, sub, and but needs to test

--- instructions/instructions.h
+++ instructions/instructions.h
@@ -5,8 +5,8 @@
 /*
  *  Ana    -> 4*movs
  *  Lúcio  -> *cmp*, -jmp-, *sar* // TODO revisar asteriscos
- *  Maiara -> add, sub, and
- *  Paulo  -> jmpg, jmpl, jmpe //TODO revisar isso aqui assing: Lúcio
+ *  Maiara -> *add*, *sub*, *and*
+ *  Paulo  -> *jmpg*, *jmpl*, *jmpe* //TODO revisar isso aqui assing: Lúcio
  *  Rafa   -> xor, or, sal
  * */
 
@@ -15,6 +15,9 @@ void jmp(Cpu* cpu, uint16_t thirdField);
 void jmp_g(Cpu* cpu, uint16_t thirdField);
 void jmp_l(Cpu* cpu, uint16_t thirdField);
 void jmp_e(Cpu* cpu, uint16_t thirdField);
+void add(Cpu* cpu, uint8_t firstField, uint8_t secondField);
+void sub(Cpu* cpu, uint8_t firstField, uint8_t secondField);
+void aNd(Cpu* cpu, uint8_t firstField, uint8_t secondField);
 void sar(Cpu* cpu,uint8_t firstField, uint16_t thirdField);
 int exit_setup(Cpu* cpu);
 
