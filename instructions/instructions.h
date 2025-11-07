c9d70c0a62c101d7aeaaf70dd64f478e35fb2a08
luciomelo1407 <luciomelo90@gmail.com>
2025-11-06 21:03:47 -0300

    merge junto tando tudo de todos e solucionando alguns bugs

--- instructions/instructions.h
+++ instructions/instructions.h
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #ifndef instructions_h
 #define instructions_h
+/*  //WARN: Lúcio verifica a porra das extensões de sinais se nn vai dar uma merda do krlh; */
 /*
  *  Ana    -> 4*movs
  *  Lúcio  -> *cmp*, -jmp-, *sar* // TODO revisar asteriscos
@@ -10,6 +11,10 @@
  *  Rafa   -> xor, or, sal
  * */
 
+void mov0(Cpu* cpu, uint8_t firstField,uint16_t thirdField);
+void mov01(Cpu *cpu, uint8_t firstField, uint8_t secondField);
+void mov02(Cpu* cpu, uint8_t firstField, uint8_t secondField);
+void mov03(Cpu *cpu, uint8_t firstField, uint8_t secondField);
 void cmp(Cpu* cpu,uint8_t firstField, uint8_t secondField );
 void jmp(Cpu* cpu, uint16_t thirdField);
 void jmp_g(Cpu* cpu, uint16_t thirdField);
@@ -18,6 +23,9 @@ void jmp_e(Cpu* cpu, uint16_t thirdField);
 void add(Cpu* cpu, uint8_t firstField, uint8_t secondField);
 void sub(Cpu* cpu, uint8_t firstField, uint8_t secondField);
 void aNd(Cpu* cpu, uint8_t firstField, uint8_t secondField);
+void oR(Cpu* cpu,uint8_t firstField, uint8_t secondField);
+void xOr(Cpu* cpu,uint8_t firstField, uint8_t secondField);
+void sal(Cpu* cpu,uint8_t firstField, uint16_t thirdField);
 void sar(Cpu* cpu,uint8_t firstField, uint16_t thirdField);
 int exit_setup(Cpu* cpu);
 
