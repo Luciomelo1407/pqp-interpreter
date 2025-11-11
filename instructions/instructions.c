4dc7a870b9cf89e4282f8d215368edde60ffb887
luciomelo1407 <luciomelo90@gmail.com>
2025-11-11 13:45:45 -0300

    final

--- instructions/instructions.c
+++ instructions/instructions.c
@@ -1,7 +1,6 @@
 #include "instructions.h"
 #include <stdint.h>
 #include <stdio.h>
-// WARN: SAIR INCREMENTANDO O DUPLICATED COUNTER EM TD MUNDO
 void checkDuplicated(Cpu *cpu, uint8_t instructionCode, bool *print) {
   for (int i = 0; i < cpu->duplicateCounter[instructionCode]; i++) {
     if (cpu->duplicate[instructionCode][i] == cpu->pc) {
@@ -18,8 +17,8 @@ void mov0(Cpu *cpu, uint8_t firstField, uint16_t thirdField) {
   bool print = true;
   checkDuplicated(cpu, 0x00, &print);
   if (print) {
+    cpu->duplicate[0x00][cpu->duplicateCounter[0x00]] = cpu->pc;
     cpu->duplicateCounter[0x00]++;
-    cpu->duplicate[0x00][cpu->duplicateCounter[0x00] - 1] = cpu->pc;
     fprintf(cpu->output, "0x%04X->MOV_R%d=0x%08X\n", cpu->pc, firstField,
             cpu->registers[firstField]);
   }
@@ -31,10 +30,35 @@ void mov01(Cpu *cpu, uint8_t firstField, uint8_t secondField) {
   bool print = true;
   checkDuplicated(cpu, 0x01, &print);
   if (print) {
-    cpu->duplicate[0x01][cpu->duplicateCounter[0x01] - 1] = cpu->pc;
+    cpu->duplicate[0x01][cpu->duplicateCounter[0x01]] = cpu->pc;
     cpu->duplicateCounter[0x01]++;
     fprintf(cpu->output, "0x%04X->MOV_R%d=R%d=0x%08X\n", cpu->pc, firstField,
             secondField, cpu->registers[firstField]);
+  }
+}
+
+void mov02(Cpu *cpu, uint8_t firstField, uint8_t secondField) {
+  cpu->registers[firstField] =
+      ((uint32_t)cpu->mem[cpu->registers[secondField] + 3]) << 24 |
+      ((uint32_t)cpu->mem[cpu->registers[secondField] + 2]) << 16 |
+      ((uint32_t)cpu->mem[cpu->registers[secondField] + 1]) << 8 |
+      ((uint32_t)cpu->mem[cpu->registers[secondField]]);
+  cpu->instructionsCounter[0x02]++;
+  bool print = true;
+  checkDuplicated(cpu, 0x02, &print);
+  if (print) {
+    cpu->duplicate[0x02][cpu->duplicateCounter[0x02]] = cpu->pc;
+    cpu->duplicateCounter[0x02]++;
+    fprintf(
+        cpu->output,
+        "0x%04X->MOV_R%d=MEM[0x%02X,0x%02X,0x%02X,0x%02X]=[0x%02X,0x%02X,0x%"
+        "02X,0x%02X]\n",
+        cpu->pc, firstField, cpu->registers[secondField],
+        cpu->registers[secondField] + 1, cpu->registers[secondField] + 2,
+        cpu->registers[secondField] + 3, cpu->mem[cpu->registers[secondField]],
+        cpu->mem[cpu->registers[secondField] + 1],
+        cpu->mem[cpu->registers[secondField] + 2],
+        cpu->mem[cpu->registers[secondField] + 3]);
   }
 }
 
@@ -51,53 +75,19 @@ void mov03(Cpu *cpu, uint8_t firstField, uint8_t secondField) {
   cpu->instructionsCounter[0x03]++;
   bool print = true;
   checkDuplicated(cpu, 0x03, &print);
-  // printf("\nDEBUG: ");
-  // printf("0x%04X->MOV_MEM[0x%02X,0x%02X,0x%02X,0x%02X]=R%d=[0x%02X,0x%02X,0x%"
-  //        "02X,0x%02X]\n",
-  //        cpu->pc, cpu->registers[firstField], cpu->registers[firstField] + 1,
-  //        cpu->registers[firstField] + 2, cpu->registers[firstField] + 3,
-  //        secondField, cpu->mem[cpu->registers[firstField] + 3],
-  //        cpu->mem[cpu->registers[firstField] + 2],
-  //        cpu->mem[cpu->registers[firstField] + 1],
-  //        cpu->mem[cpu->registers[firstField]]);
   if (print) {
+    cpu->duplicate[0x03][cpu->duplicateCounter[0x03]] = cpu->pc;
     cpu->duplicateCounter[0x03]++;
-    cpu->duplicate[0x03][cpu->duplicateCounter[0x03] - 1] = cpu->pc;
     fprintf(
         cpu->output,
         "0x%04X->MOV_MEM[0x%02X,0x%02X,0x%02X,0x%02X]=R%d=[0x%02X,0x%02X,0x%"
         "02X,0x%02X]\n",
         cpu->pc, cpu->registers[firstField], cpu->registers[firstField] + 1,
         cpu->registers[firstField] + 2, cpu->registers[firstField] + 3,
-        secondField, cpu->mem[cpu->registers[firstField] + 3],
-        cpu->mem[cpu->registers[firstField] + 2],
+        secondField, cpu->mem[cpu->registers[firstField]],
         cpu->mem[cpu->registers[firstField] + 1],
-        cpu->mem[cpu->registers[firstField]]);
-  }
-}
-
-void mov02(Cpu *cpu, uint8_t firstField, uint8_t secondField) {
-  cpu->registers[firstField] =
-      ((uint32_t)cpu->mem[cpu->registers[secondField] + 3]) << 24 |
-      ((uint32_t)cpu->mem[cpu->registers[secondField] + 2]) << 16 |
-      ((uint32_t)cpu->mem[cpu->registers[secondField] + 1]) << 8 |
-      ((uint32_t)cpu->mem[cpu->registers[secondField]]);
-  cpu->instructionsCounter[0x02]++;
-  bool print = true;
-  checkDuplicated(cpu, 0x02, &print);
-  if (print) {
-    cpu->duplicate[0x02][cpu->duplicateCounter[0x02] - 1] = cpu->pc;
-    cpu->duplicateCounter[0x02]++;
-    fprintf(
-        cpu->output,
-        "0x%04X->MOV_R%d=MEM[0x%02X,0x%02X,0x%02X,0x%02X]=[0x%02X,0x%02X,0x%"
-        "02X,0x%02X]\n",
-        cpu->pc, firstField, cpu->registers[secondField],
-        cpu->registers[secondField] + 1, cpu->registers[secondField] + 2,
-        cpu->registers[secondField] + 3, cpu->mem[cpu->registers[secondField]],
-        cpu->mem[cpu->registers[secondField] + 1],
-        cpu->mem[cpu->registers[secondField] + 2],
-        cpu->mem[cpu->registers[secondField] + 3]);
+        cpu->mem[cpu->registers[firstField] + 2],
+        cpu->mem[cpu->registers[firstField] + 3]);
   }
 }
 
@@ -120,7 +110,7 @@ void cmp(Cpu *cpu, uint8_t firstField, uint8_t secondField) {
   bool print = true;
   checkDuplicated(cpu, 0x04, &print);
   if (print) {
-    cpu->duplicate[0x04][cpu->duplicateCounter[0x04] - 1] = cpu->pc;
+    cpu->duplicate[0x04][cpu->duplicateCounter[0x04]] = cpu->pc;
     cpu->duplicateCounter[0x04]++;
     fprintf(cpu->output, "0x%04X->CMP_R%d<=>R%d(G=%d,L=%d,E=%d)\n", cpu->pc,
             firstField, secondField, cpu->g, cpu->l, cpu->e);
@@ -132,12 +122,12 @@ void jmp(Cpu *cpu, uint16_t thirdField) {
   checkDuplicated(cpu, 0x05, &print);
   cpu->instructionsCounter[0x05]++;
   if (print) {
-    cpu->duplicate[0x05][cpu->duplicateCounter[0x05] - 1] = cpu->pc;
+    cpu->duplicate[0x05][cpu->duplicateCounter[0x05]] = cpu->pc;
     cpu->duplicateCounter[0x05]++;
     fprintf(cpu->output, "0x%04X->JMP_0x%04X\n", cpu->pc,
             cpu->pc + (int16_t)thirdField + 4);
   }
-  cpu->pc += (int16_t)thirdField;
+  cpu->pc += (int16_t)thirdField; // teste
 }
 
 void jmp_g(Cpu *cpu, uint16_t thirdField) {
@@ -146,7 +136,7 @@ void jmp_g(Cpu *cpu, uint16_t thirdField) {
   cpu->instructionsCounter[0x06]++;
   if (cpu->g) {
     if (print) {
-      cpu->duplicate[0x06][cpu->duplicateCounter[0x06] - 1] = cpu->pc;
+      cpu->duplicate[0x06][cpu->duplicateCounter[0x06]] = cpu->pc;
       cpu->duplicateCounter[0x06]++;
       fprintf(cpu->output, "0x%04X->JG_0x%04X\n", cpu->pc,
               cpu->pc + (int16_t)thirdField + 4);
@@ -154,7 +144,7 @@ void jmp_g(Cpu *cpu, uint16_t thirdField) {
     cpu->pc += (int16_t)thirdField;
   } else {
     if (print) {
-      cpu->duplicate[0x06][cpu->duplicateCounter[0x06] - 1] = cpu->pc;
+      cpu->duplicate[0x06][cpu->duplicateCounter[0x06]] = cpu->pc;
       cpu->duplicateCounter[0x06]++;
       fprintf(cpu->output, "0x%04X->JG_0x%04X\n", cpu->pc, cpu->pc + 4);
     }
@@ -168,7 +158,7 @@ void jmp_l(Cpu *cpu, uint16_t thirdField) {
   cpu->instructionsCounter[0x07]++;
   if (cpu->l) {
     if (print) {
-      cpu->duplicate[0x07][cpu->duplicateCounter[0x07] - 1] = cpu->pc;
+      cpu->duplicate[0x07][cpu->duplicateCounter[0x07]] = cpu->pc;
       cpu->duplicateCounter[0x07]++;
       fprintf(cpu->output, "0x%04X->JL_0x%04X\n", cpu->pc,
               cpu->pc + (int16_t)thirdField + 4);
@@ -176,7 +166,7 @@ void jmp_l(Cpu *cpu, uint16_t thirdField) {
     cpu->pc += (int16_t)thirdField;
   } else {
     if (print) {
-      cpu->duplicate[0x07][cpu->duplicateCounter[0x07] - 1] = cpu->pc;
+      cpu->duplicate[0x07][cpu->duplicateCounter[0x07]] = cpu->pc;
       cpu->duplicateCounter[0x07]++;
       fprintf(cpu->output, "0x%04X->JL_0x%04X\n", cpu->pc, cpu->pc + 4);
     }
@@ -189,17 +179,23 @@ void jmp_e(Cpu *cpu, uint16_t thirdField) {
   checkDuplicated(cpu, 0x08, &print);
   cpu->instructionsCounter[0x08]++;
   if (cpu->e) {
-    printf("0x%04X->JE_0x%04X\n", cpu->pc, cpu->pc + (uint16_t)thirdField + 4);
+    printf("\nfora: thirdField : %x %x %x\n", thirdField, (int16_t)thirdField,
+           (int32_t)(int16_t)thirdField);
+    int32_t printsSumWithPc = cpu->pc + (uint16_t)thirdField + 4;
+    printf("fora: 0x%04X->JE_0x%04X\n", cpu->pc, printsSumWithPc);
     if (print) {
-      cpu->duplicate[0x08][cpu->duplicateCounter[0x08] - 1] = cpu->pc;
+      cpu->duplicate[0x08][cpu->duplicateCounter[0x08]] = cpu->pc;
       cpu->duplicateCounter[0x08]++;
-      fprintf(cpu->output, "0x%04X->JE_0x%04X\n", cpu->pc,
-              cpu->pc + (int16_t)thirdField + 4);
+      fprintf(cpu->output, "0x%04X->JE_0x%04X\n", cpu->pc, printsSumWithPc);
+      printf("\ndento: thirdField : %x %x %x\n", thirdField,
+             (int16_t)thirdField, (int32_t)(int16_t)thirdField);
+      printf("dento: 0x%04X->JE_0x%04X\n", cpu->pc,
+             cpu->pc + (int32_t)(int16_t)thirdField + 4);
     }
     cpu->pc += thirdField;
   } else {
     if (print) {
-      cpu->duplicate[0x08][cpu->duplicateCounter[0x08] - 1] = cpu->pc;
+      cpu->duplicate[0x08][cpu->duplicateCounter[0x08]] = cpu->pc;
       cpu->duplicateCounter[0x08]++;
       fprintf(cpu->output, "0x%04X->JE_0x%04X\n", cpu->pc, cpu->pc + 4);
     }
@@ -214,7 +210,7 @@ void add(Cpu *cpu, uint8_t firstField, uint8_t secondField) {
   bool print = true;
   checkDuplicated(cpu, 0x09, &print);
   if (print) {
-    cpu->duplicate[0x09][cpu->duplicateCounter[0x09] - 1] = cpu->pc;
+    cpu->duplicate[0x09][cpu->duplicateCounter[0x09]] = cpu->pc;
     cpu->duplicateCounter[0x09]++;
     fprintf(cpu->output, "0x%04X->ADD_R%d+=R%d=0x%08X+0x%08X=0x%08X\n", cpu->pc,
             firstField, secondField, rx, ry, cpu->registers[firstField]);
@@ -228,7 +224,7 @@ void sub(Cpu *cpu, uint8_t firstField, uint8_t secondField) {
   bool print = true;
   checkDuplicated(cpu, 0x0a, &print);
   if (print) {
-    cpu->duplicate[0x0a][cpu->duplicateCounter[0x0a] - 1] = cpu->pc;
+    cpu->duplicate[0x0a][cpu->duplicateCounter[0x0a]] = cpu->pc;
     cpu->duplicateCounter[0x0a]++;
     fprintf(cpu->output, "0x%04X->SUB_R%d-=R%d=0x%08X-0x%08X=0x%08X\n", cpu->pc,
             firstField, secondField, rx, ry, cpu->registers[firstField]);
@@ -243,7 +239,7 @@ void aNd(Cpu *cpu, uint8_t firstField, uint8_t secondField) {
   bool print = true;
   checkDuplicated(cpu, 0x0b, &print);
   if (print) {
-    cpu->duplicate[0x0b][cpu->duplicateCounter[0x0b] - 1] = cpu->pc;
+    cpu->duplicate[0x0b][cpu->duplicateCounter[0x0b]] = cpu->pc;
     cpu->duplicateCounter[0x0b]++;
     fprintf(cpu->output, "0x%04X->AND_R%d&=R%d=0x%08X&0x%08X=0x%08X\n", cpu->pc,
             firstField, secondField, rx, ry, cpu->registers[firstField]);
@@ -258,7 +254,7 @@ void oR(Cpu *cpu, uint8_t firstField, uint8_t secondField) {
   bool print = true;
   checkDuplicated(cpu, 0x0c, &print);
   if (print) {
-    cpu->duplicate[0x0c][cpu->duplicateCounter[0x0c] - 1] = cpu->pc;
+    cpu->duplicate[0x0c][cpu->duplicateCounter[0x0c]] = cpu->pc;
     cpu->duplicateCounter[0x0c]++;
     fprintf(cpu->output, "0x%04X->OR_R%d|=R%d=0x%08X|0x%08X=0x%08X\n", cpu->pc,
             firstField, secondField, rx, ry, cpu->registers[firstField]);
@@ -273,7 +269,7 @@ void xOr(Cpu *cpu, uint8_t firstField, uint8_t secondField) {
   bool print = true;
   checkDuplicated(cpu, 0x0d, &print);
   if (print) {
-    cpu->duplicate[0x0d][cpu->duplicateCounter[0x0d] - 1] = cpu->pc;
+    cpu->duplicate[0x0d][cpu->duplicateCounter[0x0d]] = cpu->pc;
     cpu->duplicateCounter[0x0d]++;
     fprintf(cpu->output, "0x%04X->XOR_R%d^=R%d=0x%08X^0x%08X=0x%08X\n", cpu->pc,
             firstField, secondField, rx, ry, cpu->registers[firstField]);
@@ -288,7 +284,7 @@ void sar(Cpu *cpu, uint8_t firstField, uint16_t thirdField) {
   bool print = true;
   checkDuplicated(cpu, 0x0f, &print);
   if (print) {
-    cpu->duplicate[0x0f][cpu->duplicateCounter[0x0f] - 1] = cpu->pc;
+    cpu->duplicate[0x0f][cpu->duplicateCounter[0x0f]] = cpu->pc;
     cpu->duplicateCounter[0x0f]++;
     fprintf(cpu->output, "0x%04X->SAR_R%d>>=%d=0x%08X>>%d=0x%08X\n", cpu->pc,
             firstField, i, before, i, cpu->registers[firstField]);
@@ -303,7 +299,7 @@ void sal(Cpu *cpu, uint8_t firstField, uint16_t thirdField) {
   bool print = true;
   checkDuplicated(cpu, 0x0e, &print);
   if (print) {
-    cpu->duplicate[0x0e][cpu->duplicateCounter[0x0e] - 1] = cpu->pc;
+    cpu->duplicate[0x0e][cpu->duplicateCounter[0x0e]] = cpu->pc;
     cpu->duplicateCounter[0x0e]++;
     fprintf(cpu->output, "0x%04X->SAL_R%d<<=%d=0x%08X<<%d=0x%08X\n", cpu->pc,
             firstField, i, before, i, cpu->registers[firstField]);
